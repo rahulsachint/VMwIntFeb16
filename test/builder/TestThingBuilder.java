8aaea03e6c40c105faaab288cb08630d3134d988
SimonHGR <simon@dancingcloudservices.com>
2016-02-02 14:24:43 -0800

    Added test source tree for Builder example

---
+++ test/builder/TestThingBuilder.java
@@ -0,0 +1,38 @@
+package builder;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author simon
+ */
+public class TestThingBuilder {
+    @Test(expected=IllegalArgumentException.class)
+    public void uninitializedShouldFail() {
+        Thing.builder().build();
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void xGreaterYShouldFail() {
+        Thing.builder().x(5).y(3).build();
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void uninitializedStringSShouldFail() {
+        Thing.builder().x(3).y(5).t("Hello").build();
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void uninitializedStringTShouldFail() {
+        Thing.builder().x(3).y(5).s("Hello").build();
+    }
+    
+        @Test()
+    public void allGoodShouldPass() {
+        Thing.builder().x(3).y(5).s("What?").t("Hello").build();
+    }
+    
+
+    
+    
+}
