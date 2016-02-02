e09095186e78c988aee8fb84e8da3abbb7b94333
SimonHGR <simon@dancingcloudservices.com>
2016-02-02 14:57:12 -0800

    Cleanup + Assert.assertEquals example

--- test/builder/TestThingBuilder.java
+++ test/builder/TestThingBuilder.java
@@ -1,5 +1,6 @@
 package builder;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -7,32 +8,35 @@ import org.junit.Test;
  * @author simon
  */
 public class TestThingBuilder {
-    @Test(expected=IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void uninitializedShouldFail() {
         Thing.builder().build();
     }
-    
-    @Test(expected=IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void xGreaterYShouldFail() {
         Thing.builder().x(5).y(3).build();
     }
-    
-    @Test(expected=IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void uninitializedStringSShouldFail() {
         Thing.builder().x(3).y(5).t("Hello").build();
     }
-    
-    @Test(expected=IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void uninitializedStringTShouldFail() {
         Thing.builder().x(3).y(5).s("Hello").build();
     }
-    
-        @Test()
+
+    @Test
     public void allGoodShouldPass() {
         Thing.builder().x(3).y(5).s("What?").t("Hello").build();
     }
-    
 
-    
-    
+    @Test
+    public void testStringLength() {
+        String l = "Hello";
+        Assert.assertEquals("Length should be five", 6, l.length());
+    }
 }
